10c4ccf5439bfa7ff63f1c0506a947515550f148
khristova21 <42977135+khristova21@users.noreply.github.com>
2018-09-12 13:07:00 -0400

    Add files via upload

---
+++ src/SmileyFace.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ * @author Krisi Hristova
+ *Smiley Face
+ */
+public class SmileyFace 
+{
+
+	public static void main(String[] args)
+	{
+		
+			System.out.println("/////////");
+			System.out.println("| *   * |");
+			System.out.println("|   *   |");
+			System.out.println("| \\___/ |");
+			System.out.println("\\_______/");
+
+
+
+	}
+
+}
